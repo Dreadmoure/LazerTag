5b63acbfffb0f1d9a1e0513e8ff5958da8852a99
denniwind <denniwind@live.dk>
2023-05-04 12:42:51 +0200

    Added * PlayerBuilder * Player * RedIdle0.png

---
+++ LazerTag/BuilderPattern/PlayerBuilder.cs
@@ -0,0 +1,46 @@
+﻿using LazerTag.ComponentPattern;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazerTag.BuilderPattern
+{
+    public class PlayerBuilder : IBuilder
+    {
+        private GameObject gameObject;
+        private int id;
+
+        public PlayerBuilder(int id)
+        {
+            this.id = id;
+        }
+
+        public void BuildGameObject()
+        {
+            gameObject = new GameObject();
+
+            BuildComponents();
+        }
+
+        /// <summary>
+        /// adds components to the gameobject
+        /// </summary>
+        private void BuildComponents()
+        {
+            Player player = (Player)gameObject.AddComponent(new Player());
+            player.Id = id;
+        }
+
+        /// <summary>
+        /// Method for returning the gameobject
+        /// </summary>
+        /// <returns>GameObject</returns>
+        public GameObject GetResult()
+        {
+            return gameObject;
+        }
+    }
+}

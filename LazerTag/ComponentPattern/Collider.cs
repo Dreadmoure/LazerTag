d2097878a7c804dddcbc4ef090968be5cdeb4d29
IdaDania <54858@student.eadania.dk>
2023-05-05 00:01:22 +0200

    Collider.cs : Modified * added properties for side collision events * inserted method DrawRectangle from other project, to draw the collisionboxes * method CheckCollision now notifies for side collisionevents * added methods to check if a side is touched

--- LazerTag/ComponentPattern/Collider.cs
+++ LazerTag/ComponentPattern/Collider.cs
@@ -1,4 +1,5 @@
 ﻿using LazerTag.ObserverPattern;
+using LazerTag.ObserverPattern.PlatformCollisionEvents;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -21,6 +22,10 @@ namespace LazerTag.ComponentPattern
         /// used to get or set the collsion event
         /// </summary>
         public CollisionEvent CollisionEvent { get; private set; } = new CollisionEvent();
+        public TopCollisionEvent TopCollisionEvent { get; private set; } = new TopCollisionEvent();
+        public BottomCollisionEvent BottomCollisionEvent { get; private set; } = new BottomCollisionEvent();
+        public LeftCollisionEvent LeftCollisionEvent { get; private set; } = new LeftCollisionEvent();
+        public RightCollisionEvent RightCollisionEvent { get; private set; } = new RightCollisionEvent();
 
         /// <summary>
         /// used to return a rectangle based on the objects position and its sprite
@@ -47,7 +52,7 @@ namespace LazerTag.ComponentPattern
         public override void Start()
         {
             spriteRenderer = (SpriteRenderer)GameObject.GetComponent<SpriteRenderer>();
-            texture = GameWorld.Instance.Content.Load<Texture2D>("Sprites\\Pixel");
+            texture = GameWorld.Instance.Content.Load<Texture2D>("Pixel");
         }
 
         /// <summary>
@@ -61,10 +66,23 @@ namespace LazerTag.ComponentPattern
         }
 
         //use this if you want to draw the collisionboxes
-        //public override void Draw(SpriteBatch spriteBatch)
-        //{
-        //    DrawRectangle(CollisionBox, spriteBatch);
-        //}
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            DrawRectangle(CollisionBox, spriteBatch);
+        }
+
+        public void DrawRectangle(Rectangle collisionBox, SpriteBatch spriteBatch)
+        {
+            Rectangle topLine = new Rectangle(collisionBox.X, collisionBox.Y, collisionBox.Width, 1);
+            Rectangle bottomLine = new Rectangle(collisionBox.X, collisionBox.Y + collisionBox.Height, collisionBox.Width, 1);
+            Rectangle rightLine = new Rectangle(collisionBox.X + collisionBox.Width, collisionBox.Y, 1, collisionBox.Height);
+            Rectangle leftLine = new Rectangle(collisionBox.X, collisionBox.Y, 1, collisionBox.Height);
+
+            spriteBatch.Draw(texture, topLine, null, Color.Red, 0f, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(texture, bottomLine, null, Color.Red, 0f, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(texture, rightLine, null, Color.Red, 0f, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(texture, leftLine, null, Color.Red, 0f, Vector2.Zero, SpriteEffects.None, 1);
+        }
 
         /// <summary>
         /// method for checking if objects collide
@@ -73,11 +91,57 @@ namespace LazerTag.ComponentPattern
         {
             foreach (Collider other in GameWorld.Instance.Colliders)
             {
-                if (other != this && other.CollisionBox.Contains(CollisionBox))
+                if (other != this && other.CollisionBox.Intersects(CollisionBox))
                 {
                     CollisionEvent.Notify(other.GameObject);
+
+                    if(TouchTopOf(CollisionBox, other.CollisionBox))
+                    {
+                        TopCollisionEvent.Notify(other.GameObject); 
+                    }
+                    if (TouchBottomOf(CollisionBox, other.CollisionBox))
+                    {
+                        BottomCollisionEvent.Notify(other.GameObject);
+                    }
+                    if (TouchLeftOf(CollisionBox, other.CollisionBox))
+                    {
+                        LeftCollisionEvent.Notify(other.GameObject);
+                    }
+                    if (TouchRightOf(CollisionBox, other.CollisionBox))
+                    {
+                        RightCollisionEvent.Notify(other.GameObject);
+                    }
                 }
             }
+        }
+
+        private bool TouchTopOf(Rectangle r1, Rectangle r2)
+        {
+            return (r1.Bottom >= r2.Top - 5 &&
+                    r1.Bottom <= r2.Top + 5 &&
+                    r1.Right >= r2.Left &&
+                    r1.Left <= r2.Right); 
+        }
+        private bool TouchBottomOf(Rectangle r1, Rectangle r2)
+        {
+            return (r1.Top <= r2.Bottom + 10 &&
+                    r1.Top >= r2.Bottom - 10 &&
+                    r1.Right >= r2.Left + 7 &&
+                    r1.Left <= r2.Right - 7);
+        }
+        private bool TouchLeftOf(Rectangle r1, Rectangle r2)
+        {
+            return (r1.Right <= r2.Left + 5 &&
+                    r1.Right >= r2.Left - 5 &&
+                    r1.Top <= r2.Bottom - 5 &&
+                    r1.Bottom >= r2.Top + 5);
+        }
+        private bool TouchRightOf(Rectangle r1, Rectangle r2)
+        {
+            return (r1.Left >= r2.Right - 5 &&
+                    r1.Left <= r2.Right + 5 &&
+                    r1.Top <= r2.Bottom - 5 &&
+                    r1.Bottom >= r2.Top + 5);
         }
         #endregion
     }

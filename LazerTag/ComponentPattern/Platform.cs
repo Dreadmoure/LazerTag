530411ac760004b9c8c9c809001245c5bd70efee
IdaDania <54858@student.eadania.dk>
2023-05-04 23:58:44 +0200

    Platform.cs : Added * platforms get a position, given through the builder, and is multiplied with the sprite width and height

---
+++ LazerTag/ComponentPattern/Platform.cs
@@ -0,0 +1,43 @@
+﻿using LazerTag.ObserverPattern;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazerTag.ComponentPattern
+{
+    public class Platform : Component
+    {
+        private SpriteRenderer spriteRenderer;
+        private Vector2 position; 
+
+        public Platform(Vector2 position)
+        {
+            this.position = position; 
+        }
+
+        #region methods 
+        public override void Awake()
+        {
+            spriteRenderer = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
+            spriteRenderer.SetSprite("Platforms\\tile1");
+            spriteRenderer.Scale = 1;
+            spriteRenderer.LayerDepth = 0.5f;
+
+            GameObject.Transform.Position = new Vector2(position.X * spriteRenderer.Sprite.Width, position.Y * spriteRenderer.Sprite.Height);
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+        #endregion
+    }
+}

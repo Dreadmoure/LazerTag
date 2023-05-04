73ff7e31806066698af8da690013f82bc4322d07
IdaDania <54858@student.eadania.dk>
2023-05-05 00:02:56 +0200

    Character.cs : Modified * added gravity * set the spriterenderer * collisionchecks with platform * change character behavior according to side of platform touched

--- LazerTag/ComponentPattern/Character.cs
+++ LazerTag/ComponentPattern/Character.cs
@@ -1,15 +1,17 @@
 ﻿using LazerTag.CommandPattern;
 using LazerTag.ObserverPattern;
+using LazerTag.ObserverPattern.PlatformCollisionEvents;
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LazerTag.ComponentPattern
 {
-    public class Character : Component
+    public class Character : Component, IGameListener
     {
         #region fields
         private SpriteRenderer spriteRenderer;
@@ -17,6 +19,8 @@ namespace LazerTag.ComponentPattern
         private float speed;
         private bool canShoot;
         private float shootTimer;
+
+        private Vector2 gravity;
 
         //private Weapon weapon;
         #endregion
@@ -28,14 +32,19 @@ namespace LazerTag.ComponentPattern
         #region methods
         public override void Start()
         {
+            spriteRenderer = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer; 
             AmmoCount = 5;
             speed = 250;
+            gravity = new Vector2(0, 0.9f) * speed;
         }
 
         public override void Update()
         {
             //handles input
             InputHandler.Instance.Execute(this);
+
+            // make character fall using gravity 
+            GameObject.Transform.Translate(gravity * GameWorld.DeltaTime); 
         }
 
         public void Move(Vector2 velocity)
@@ -61,7 +70,71 @@ namespace LazerTag.ComponentPattern
 
         public void Notify(GameEvent gameEvent)
         {
+            if(gameEvent is CollisionEvent)
+            {
+                GameObject other = (gameEvent as CollisionEvent).Other; 
 
+                if(other.Tag == "Platform")
+                {
+                    //Debug.WriteLine("collide"); 
+                }
+            }
+            if(gameEvent is TopCollisionEvent)
+            {
+                GameObject other = (gameEvent as TopCollisionEvent).Other;
+
+                if(other.Tag == "Platform")
+                {
+                    Debug.WriteLine("top");
+
+                    SpriteRenderer otherSpriteRenderer = other.GetComponent<SpriteRenderer>() as SpriteRenderer;
+
+                    GameObject.Transform.Position = new Vector2(GameObject.Transform.Position.X, 
+                                                                other.Transform.Position.Y - (otherSpriteRenderer.Origin.Y + spriteRenderer.Origin.Y));
+                }
+            }
+            if (gameEvent is BottomCollisionEvent)
+            {
+                GameObject other = (gameEvent as BottomCollisionEvent).Other;
+
+                if (other.Tag == "Platform")
+                {
+                    Debug.WriteLine("bottom");
+
+                    SpriteRenderer otherSpriteRenderer = other.GetComponent<SpriteRenderer>() as SpriteRenderer;
+
+                    GameObject.Transform.Position = new Vector2(GameObject.Transform.Position.X,
+                                                                other.Transform.Position.Y + (otherSpriteRenderer.Origin.Y + spriteRenderer.Origin.Y));
+                }
+            }
+            if (gameEvent is LeftCollisionEvent)
+            {
+                GameObject other = (gameEvent as LeftCollisionEvent).Other;
+
+                if (other.Tag == "Platform")
+                {
+                    Debug.WriteLine("left");
+
+                    SpriteRenderer otherSpriteRenderer = other.GetComponent<SpriteRenderer>() as SpriteRenderer;
+
+                    GameObject.Transform.Position = new Vector2(other.Transform.Position.X - (otherSpriteRenderer.Origin.X + spriteRenderer.Origin.X),
+                                                                GameObject.Transform.Position.Y);
+                }
+            }
+            if (gameEvent is RightCollisionEvent)
+            {
+                GameObject other = (gameEvent as RightCollisionEvent).Other;
+
+                if (other.Tag == "Platform")
+                {
+                    Debug.WriteLine("right");
+
+                    SpriteRenderer otherSpriteRenderer = other.GetComponent<SpriteRenderer>() as SpriteRenderer;
+
+                    GameObject.Transform.Position = new Vector2(other.Transform.Position.X + (otherSpriteRenderer.Origin.X + spriteRenderer.Origin.X),
+                                                                GameObject.Transform.Position.Y);
+                }
+            }
         }
         #endregion
     }

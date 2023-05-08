b0fef987af5a8f6a87ad668c1b804260e6258910
denniwind <denniwind@live.dk>
2023-05-08 14:16:09 +0200

    Weapon.cs : added * Added weapon class

---
+++ LazerTag/ComponentPattern/Weapon.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazerTag.ComponentPattern
+{
+    public class Weapon : Component
+    {
+        private SpriteRenderer spriteRenderer;
+        private Vector2 aimDirection; //maybe enum
+        private Vector2 projectileSpawnPosition;
+
+        public override void Start()
+        {
+            spriteRenderer = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
+
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public void Move(Vector2 position)
+        {
+            if (aimDirection.Y == -1)
+            {
+                spriteRenderer.Flip = SpriteEffects.None;
+                GameObject.Transform.Position = position + new Vector2(0, -30);
+                GameObject.Transform.Rotation = -1.6f;
+            }
+            else if (aimDirection.Y == 1)
+            {
+                spriteRenderer.Flip = SpriteEffects.None;
+                GameObject.Transform.Position = position + new Vector2(0, 30);
+                GameObject.Transform.Rotation = 1.6f;
+            }
+
+            else if (aimDirection.X == -1)
+            {
+                spriteRenderer.Flip = SpriteEffects.FlipHorizontally;
+                GameObject.Transform.Position = position + new Vector2(-30, 0);
+                GameObject.Transform.Rotation = 0f;
+            }
+            else
+            {
+                spriteRenderer.Flip = SpriteEffects.None;
+                GameObject.Transform.Position = position + new Vector2(30, 0);
+                GameObject.Transform.Rotation = 0f;
+            }
+
+        }
+
+        public void Aim(Vector2 aimDirection)
+        {
+            this.aimDirection = aimDirection;
+        }
+    }
+}

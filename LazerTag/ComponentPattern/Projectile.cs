7f2d48df96da08d9609ba8eeab5ed5a8136ac54d
denniwind <denniwind@live.dk>
2023-05-08 14:15:23 +0200

    Projectile.cs : Added * added class for the projectile

---
+++ LazerTag/ComponentPattern/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using LazerTag.ObserverPattern;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazerTag.ComponentPattern
+{
+    public class Projectile : Component, IGameListener
+    {
+        private float speed;
+        private Vector2 velocity;
+
+        public Projectile()
+        {
+
+        }
+
+        public override void Start()
+        {
+            
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        private void Move()
+        {
+
+        }
+
+        public void Notify(GameEvent gameEvent)
+        {
+            
+        }
+    }
+}

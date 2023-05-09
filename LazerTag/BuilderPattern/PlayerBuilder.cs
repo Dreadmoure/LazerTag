a933f835cb51ce3f1f8dfc9cccc6b2369998b4d9
IdaMarcherJensen <54858@student.eadania.dk>
2023-05-09 12:05:50 +0200

    PlayerIndex Enum : Modified * changed player ids to use PlayerIndex enum instead * set Tags on the gameobjects to match PlayerIndex

--- LazerTag/BuilderPattern/PlayerBuilder.cs
+++ LazerTag/BuilderPattern/PlayerBuilder.cs
@@ -32,27 +32,27 @@ namespace LazerTag.BuilderPattern
         private void BuildComponents()
         {
             Player player = (Player)gameObject.AddComponent(new Player());
-            player.Id = id;
+            player.Type = (PlayerIndex)id;
             
 
             UIRenderer uIRenderer = (UIRenderer)gameObject.AddComponent(new UIRenderer());
 
-            if(player.Id == 1)
+            if(player.Type == PlayerIndex.One)
             {
                 uIRenderer.SetSprite("PlayerBoxes\\RedPlayerBox", new Vector2(GameWorld.ScreenSize.X / 8, GameWorld.ScreenSize.Y / 12));
                 
             }
-            else if (player.Id == 2)
+            else if (player.Type == PlayerIndex.Two)
             {
                 uIRenderer.SetSprite("PlayerBoxes\\BluePlayerBox", new Vector2(GameWorld.ScreenSize.X / 3, GameWorld.ScreenSize.Y / 12));
 
             }
-            else if (player.Id == 3)
+            else if (player.Type == PlayerIndex.Three)
             {
                 uIRenderer.SetSprite("PlayerBoxes\\GreenPlayerBox", new Vector2(GameWorld.ScreenSize.X / 1.8f, GameWorld.ScreenSize.Y / 12));
 
             }
-            else if (player.Id == 4)
+            else if (player.Type == PlayerIndex.Four)
             {
                 uIRenderer.SetSprite("PlayerBoxes\\PinkPlayerBox", new Vector2(GameWorld.ScreenSize.X / 1.3f, GameWorld.ScreenSize.Y / 12));
 

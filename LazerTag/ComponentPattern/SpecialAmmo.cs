e9a9a00808ef240d4352e26aaaa8b287109405c3
Denni Wind <denniwind@live.dk>
2023-05-13 22:59:47 +0200

    SpecialAmmo.cs : Added * added special ammo class/component

---
+++ LazerTag/ComponentPattern/SpecialAmmo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazerTag.ComponentPattern
+{
+    public class SpecialAmmo : PickUp
+    {
+        /// <summary>
+        /// first method to be run, when the object is first initialized in GameWorld 
+        /// </summary>
+        public override void Start()
+        {
+            spriteRenderer = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
+            spriteRenderer.SetSprite("PickUps\\SpecialAmmo");
+            spriteRenderer.Scale = 1;
+            spriteRenderer.LayerDepth = 0.5f;
+            GameObject.Tag = "SpecialAmmo";
+
+            // remember to run super class method Start 
+            base.Start();
+        }
+    }
+}

6f6b7c1d5c4fa8beeae98ec68fecfbf139af50d6
Denni Wind <denniwind@live.dk>
2023-05-14 22:29:34 +0200

    SolarUpgrade.cs : Modified * Added SolarUpgrade pickup

---
+++ LazerTag/ComponentPattern/SolarUpgrade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazerTag.ComponentPattern
+{
+    public class SolarUpgrade : PickUp
+    {
+        /// <summary>
+        /// first method to be run, when the object is first initialized in GameWorld 
+        /// </summary>
+        public override void Start()
+        {
+            spriteRenderer = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
+            spriteRenderer.SetSprite("PickUps\\SolarUpgrade");
+            spriteRenderer.Scale = 1;
+            spriteRenderer.LayerDepth = 0.5f;
+            GameObject.Tag = "SolarUpgrade";
+
+            // remember to run super class method Start 
+            base.Start();
+        }
+    }
+}

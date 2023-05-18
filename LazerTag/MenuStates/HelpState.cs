73d0e9a762f528e4b1d77850ddcab3f95be2a171
IdaDania <54858@student.eadania.dk>
2023-05-18 19:52:14 +0200

    HelpState.cs : Added * a menu reached from the Main Menu, to see the controls * control scema/image has to be added

---
+++ LazerTag/MenuStates/HelpState.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazerTag.MenuStates
+{
+    public class HelpState : State
+    {
+        private Button backButton;
+
+        public HelpState(ContentManager content, GraphicsDevice graphicsDevice, GameWorld game) : base(content, graphicsDevice, game)
+        {
+            Vector2 backButtonPosition = new Vector2(200, 100);
+
+            backButton = new Button(backButtonPosition, "Back", Color.White);
+        }
+
+        public override void LoadContent()
+        {
+            backButton.LoadContent(content);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            backButton.Update(gameTime);
+
+            if (backButton.isClicked)
+            {
+                backButton.isClicked = false;
+                game.ChangeState(GameWorld.Instance.MenuState);
+            }
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin(SpriteSortMode.FrontToBack, samplerState: SamplerState.PointClamp);
+
+            backButton.Draw(gameTime, spriteBatch);
+
+            spriteBatch.End();
+        }
+    }
+}

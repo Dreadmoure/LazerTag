7001e89d6b1bcf0f27de6cb53a54cab159aa4a03
denniwind <denniwind@live.dk>
2023-05-19 10:32:07 +0200

    HighScore.cs : Added * Added HighScore to the Domain

---
+++ LazerTag/Domain/HighScore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazerTag.Domain
+{
+    public class HighScore
+    {
+        #region properties
+        /// <summary>
+        /// property to get or set the ID of the HighScore
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// property to get or set the Name of the HighScore
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// property to get or set the score of the HighScore
+        /// </summary>
+        public int Score { get; set; }
+        #endregion
+    }
+}
